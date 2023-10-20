0b65e9f22fc8baea9201e867b81152b9b02e5174
rishitJ <121947502+rishitJ@users.noreply.github.com>
2023-10-20 19:28:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+random_number = 0
+quick_draw_data_set = ""
+timer_counter = 0
+drawn_sketch = ""
+answer_holder = ""
+score = 0
+
+array_1=['0','1', '2', '3', '4',]
+random_number = Math.floor((Math.random()*array_1.length)+1)
+console.log(random_number)
